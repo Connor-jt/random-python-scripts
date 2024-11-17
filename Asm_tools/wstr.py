82af60c5727bdcd2ca80fe04fd632464bb64865b
Connor J Thompson <ct770a@gmail.com>
2024-11-17 14:05:57 +1100

    update wstr script

--- Asm_tools/wstr.py
+++ Asm_tools/wstr.py
@@ -1,15 +1,11 @@
+# RUN: py "asm_tools\wstr.py"
+target_string = "Space bar was pressed!!"
 
 
-
-
-target_string = "testing line of string"
 output_string = "cExampleSTRING dw "
-
-
 for i in range(len(target_string)):
     output_string += "'"
     output_string += target_string[i]
-    output_string += "', "
-
+    output_string += "',"
 output_string += "0"
 print(output_string)
